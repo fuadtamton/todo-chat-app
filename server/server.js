c408c9af312959eaa3e47b2ff449b0a173eb719d
fuadtamton <fuadt@factweavers.com>
2019-02-18 11:29:10 +0530

    new user joining message

--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ var io = socketIO(server)
 io.on('connection', (socket) => {
     console.log("new user connected")
 
+    socket.emit('newMessage', {
+        from: "Admin",
+        text: "Welcome to the chat app",
+        createdAt: new Date().getDate()
+    })
+
+    socket.broadcast.emit('newMessage', {
+        from: "Admin",
+        text: "New user joined",
+        createdAt: new Date().getDate()
+    })
 
     socket.on('createMessage', (message) => {
         console.log('create message', message);
@@ -22,6 +33,11 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         })
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
     })
 
     socket.on('disconnect', () => {
